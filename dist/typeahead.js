52576ac9a6b3d395a21f803b6ff9d4bc63b7203f
Danny Whalen <daniel.r.whalen@gmail.com>
2013-06-04 10:47:41 -0700

    Guard the input from having a blank or undefined value set

--- dist/typeahead.js
+++ dist/typeahead.js
@@ -630,7 +630,9 @@
                 return this.$input.val();
             },
             setInputValue: function(value, silent) {
-                this.$input.val(value);
+                if (value !== "" && value !== undefined) {
+                    this.$input.val(value);
+                }
                 !silent && this._compareQueryToInputValue();
             },
             getHintValue: function() {
